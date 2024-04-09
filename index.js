2dd4fe3f00fbb01a5cf4cb108749421f82d23e16
Abhinav068 <abhinav28897@gmail.com>
2024-04-09 22:32:33 +0530

    all functionalities working

--- index.js
+++ index.js
@@ -21,50 +21,67 @@ app.get('/app/api/addresses/:phone_number', async (req, res) => {
 
     try {
         const phone_number = req.params.phone_number
-        let data = await shopify.customer.list({ limit: 5 });
+        let data = await shopify.customer.list();
         res.send(getAddress(data, phone_number));
 
     } catch (error) {
-        res.send({error})
+        res.send({ error })
     }
-
-
 })
 
 
 
-app.put('/app/api/address/update/:address_id/:address_data', (req, res) => {
+app.put('/app/api/address/update/:address_id/:address_data', async (req, res) => {
     //Update the specified address with the provided address data and set it as the default address for the customer
+    let { address_id, address_data } = req.params;
+    // 6764727369857 // tesing address_id
+    let userid = req.headers.userid || 5410823995521; //testing customer_id
+    let newAddress = await shopify.customerAddress
+        .update(+userid, +address_id, { address2: address_data });
 
-    shopify.customerAddress
-        .update(5410823995521, 6764727369857, { address2: 'New colony' })
-        .then((data) => {
-            console.log('Adresses:', data)
-        })
-        .catch((err) => console.error(err));
-
-
-    res.sendFile(__dirname + '/view/test.html')
+    res.sendFile({ newAddress })
 })
 
-app.get('/app/api/orders/:phone_number', (req, res) => {
+app.get('/app/api/orders/:phone_number', async (req, res) => {
     //Retrieve the list of orders associated with the Shopify customer identified by the provided phone number
-    shopify.order
-        .list()
-        .then((data) => {
-            console.log('order:', data)
+    // +16135550135 ----> Test phoneNo.
+    let { phone_number } = req.params;
+
+    let orders = await shopify.order.list();
+    res.send({
+        orders: orders.filter((e) => {
+            if (!e.phone) return false
+            else {
+                console.log(e.phone.slice(e.phone.length - 10, e.phone.length) == phone_number);
+
+                return e.phone.slice(e.phone.length - 10, e.phone.length) == phone_number;
+            }
         })
-        .catch((err) => console.error(err));
+    });
+
 })
 
 
-app.get('/app/api/order/:order_number', (req, res) => {
+app.get('/app/api/order/:order_number', async (req, res) => {
     // Fetch the order status and details of the products for the specified order.
+    let { order_number } = req.params;
+
+    let orders = await shopify.order.list();
+    res.send({ order: orders.filter((e) => e.order_number == order_number)[0] })
+
 
 })
-app.get('/app/api/order/cancel/:order_number', (req, res) => {
+app.get('/app/api/order/cancel/:order_number', async (req, res) => {
     //Cancel the specified order and confirm the successful cancellation.
+    
+    let { order_number } = req.params;
+    let orders = await shopify.order.list();
+    let orderid = orders.filter((e) => e.order_number == order_number)[0]?.id
 
+    let response = await shopify.order.cancel(orderid);
+  
+    if(response.cancelled_at) res.send({ orderid, message:'Order has sucessfully cancelled.' });
+    else res.send({msg:'Something went wrong.'})
 
 })
 
